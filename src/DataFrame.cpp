b146151e962cb90d0b3dd163bd138dce841cca48
Vinícius Sousa <vinicius.antunes@proton.me>
2024-03-30 15:57:34 -0300

    Update DataFrame.cpp

--- src/DataFrame.cpp
+++ src/DataFrame.cpp
@@ -7,17 +7,12 @@
 #include <unordered_map>
 #include <string>
 
-template<typename T>
-class DataFrame {
-private:
-    std::unordered_map<std::string, std::vector<T>> data;
 
-public:
-    void add_column(const std::string& column_name, const std::vector<T>& column_data) {
-        data[column_name] = column_data;
-    }
+void DataFrame::add_column(const std::string& column_name, const std::vector<T>& column_data) {
+    data[column_name] = column_data;
+}
 
-    std::vector<T> get_column(const std::string& column_name) {
-        return data[column_name];
-    }
-};
+std::vector<T> DataFrame::get_column(const std::string& column_name) {
+    return data[column_name];
+}
+}
