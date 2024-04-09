bb5296ef2d0dcdf3f4e6d3c55ce6697b62da8e04
Vinícius Sousa <vinicius.antunes@proton.me>
2024-04-09 20:49:11 -0300

    update dataframe to howl multiple types

--- src/DataFrame.h
+++ src/DataFrame.h
@@ -1,20 +1,16 @@
-//
-// Created by vinicius on 3/30/24.
-//
-
-#ifndef COMPESCA_A1_DATAFRAME_H
-#define COMPESCA_A1_DATAFRAME_H
+#include <any>
 #include <vector>
 #include <unordered_map>
 #include <string>
-
-template <typename T>
+#include <typeinfo>
 class DataFrame {
 private:
-    std::unordered_map<std::string, std::vector<T>> data;
+    std::unordered_map<std::string, std::vector<std::any>> data;
+    std::unordered_map<std::string, const std::type_info*> column_types;
     std::vector<std::string> column_order;
     int n_rows;
 public:
+    template <typename T>
     void add_column(const std::string& column_name, const std::vector<T>& column_data) {
         if (data.find(column_name) != data.end()) {
             throw std::invalid_argument("Column name already exists");
@@ -23,23 +19,52 @@ public:
             throw std::invalid_argument("Size of column data does not match number of rows");
         }
 
-        data[column_name] = column_data;
+        std::vector<std::any> any_column_data(column_data.begin(), column_data.end());
+        data[column_name] = any_column_data;
         column_order.push_back(column_name);
         n_rows = column_data.size();
-    }
 
+        column_types[column_name] = &typeid(T);
+    }
+    template <typename T>
     std::vector<T> get_column(const std::string& column_name) {
-        return data[column_name];
+        /*
+         * The function should return a vector of std::any. To get the data from std::any, you can use std::any_cast.
+         */
+        if (data.find(column_name) == data.end())
+            throw std::invalid_argument("Column name does not exist");
+        if (typeid(T) != *column_types[column_name])
+            throw std::invalid_argument("Type of value does not match type of column");
+
+        std::vector<T> column_data;
+        for (const auto& value : data[column_name]) {
+            column_data.push_back(std::any_cast<T>(value));
+        }
+
+        return column_data;
     }
 
-    void add_row(const std::vector<T>& row_data) {
-        if (row_data.size() != column_order.size()) {
+    const std::type_info* get_column_type(const std::string& column_name) {
+        if (column_types.find(column_name) == column_types.end())
+            throw std::invalid_argument("Column name does not exist");
+
+        return column_types[column_name];
+    }
+
+    void add_row(const std::initializer_list<std::any>& row_data) {
+        if (row_data.size() != column_order.size())
             throw std::invalid_argument("Size of row data does not match number of columns");
+
+        auto it = row_data.begin();
+        for (size_t i = 0; i < row_data.size(); ++i, ++it) {
+            // Check if the type of the value matches the type of the column
+            if (typeid(*it) != *column_types[column_order[i]])
+                throw std::invalid_argument("Type of value does not match type of column");
+
+            data[column_order[i]].push_back(*it);
         }
-        for (size_t i = 0; i < row_data.size(); ++i) {
-            data[column_order[i]].push_back(row_data[i]);
-        }
+
         n_rows++;
     }
+
 };
-#endif //COMPESCA_A1_DATAFRAME_H
--- test_dataframe.cpp
+++ test_dataframe.cpp
@@ -1,17 +1,19 @@
 #include <iostream>
 #include <vector>
+#include <any>
 #include "src/DataFrame.h"
 using namespace std;
 int main() {
-    DataFrame<int> df;
+    DataFrame df;
     vector<int> v = {1, 2, 3, 4, 5};
     df.add_column("column1", v);
-    vector<int> v2 = df.get_column("column1");
+    auto v2 = df.get_column<int>("column1");
     for (int i : v2) {
         cout << i << endl;
     }
-    df.add_column("column2", {6, 7, 8, 9, 10});
-    vector<int> v3 = df.get_column("column2");
+
+    df.add_column("column2", vector<int>{10, 20, 30, 40, 50});
+    auto v3 = df.get_column<int>("column2");
     for (int i : v3) {
         cout << i << endl;
     }
