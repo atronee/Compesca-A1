4f7123829e1abf575a2350af7c10fba7a2f97f17
FabricioVenturim <fabriciodalviventurim@gmail.com>
2024-04-15 20:11:29 -0300

    Adiciona leitura em bloco txt

--- src/Reader.cpp
+++ src/Reader.cpp
@@ -5,7 +5,7 @@
 #include <typeinfo>                
 #include <vector>                  
 
-void CSVReader::read(const std::string& filename, const std::vector<const std::type_info*>& types, char delimiter, ConsumerProducerQueue<std::pair<std::vector<std::string>, std::vector<std::vector<std::any>>>>& queue) {
+void CSVReader::read(const std::string& filename, const std::vector<const std::type_info*>& types, char delimiter, ConsumerProducerQueue<std::pair<std::vector<std::string>, std::vector<std::vector<std::any>>>>& queue, bool read_in_blocks) {
     std::ifstream file(filename);  // Open the CSV file for reading
     if (!file.is_open()) {          // Check if file opening failed
         std::cerr << "Error opening CSV file: " << filename << std::endl;  
@@ -26,7 +26,7 @@ void CSVReader::read(const std::string& filename, const std::vector<const std::t
         std::string cell;                // String to store each cell of the current row
 
         while (std::getline(ss, cell, delimiter)) {  // Parse each cell using delimiter
-            row.push_back(cell);                    
+            row.push_back(cell);                   
         }
 
         if (header) {                             // If it's the first row (header)
@@ -54,7 +54,7 @@ void CSVReader::read(const std::string& filename, const std::vector<const std::t
             block_data.push_back(row_data);  // Add the row data to the block data vector
             line_count++;
 
-            if (line_count == 50) {  // If block size reached or end of file
+            if (line_count == 50 && read_in_blocks) {  // If block size reached or end of file
                 // Add block data to the queue
                 queue.push({column_order, block_data});
 
@@ -70,111 +70,70 @@ void CSVReader::read(const std::string& filename, const std::vector<const std::t
 
     file.close();                        
 }
+void TXTReader::read(const std::string& filename, const std::vector<const std::type_info*>& types, char delimiter, ConsumerProducerQueue<std::pair<std::vector<std::string>, std::vector<std::vector<std::any>>>>& queue, bool read_in_blocks) {
+    std::ifstream file(filename);  // Abre o arquivo TXT para leitura
+    if (!file.is_open()) {          // Verifica se a abertura do arquivo falhou
+        std::cerr << "Erro ao abrir o arquivo TXT: " << filename << std::endl;  
+        return;                  
+    }
 
-
-// std::pair<std::vector<std::string>, std::vector<std::vector<std::any>>> CSVReader::read(const std::string& filename, const std::vector<const std::type_info*>& types, char delimiter) {
-//     /*
-//         Function to read CSV file and return data as a pair of vectors
-//     */
-//     std::ifstream file(filename);  // Open the CSV file for reading
-//     if (!file.is_open()) {          // Check if file opening failed
-//         std::cerr << "Error opening CSV file: " << filename << std::endl;  
-//         return {};                  
-//     }
-
-//     std::vector<std::string> column_order;        // Vector to store column names
-//     std::vector<std::vector<std::any>> data;      // Vector of vectors to store data
+    std::vector<std::string> column_order;        // Vetor para armazenar os nomes das colunas
+    std::vector<std::vector<std::any>> block_data; // Vetor de vetores para armazenar dados para o bloco atual
     
-//     std::string line;               // String to store each line of the CSV file
-//     bool header = true;             
-//     while (std::getline(file, line)) {  // Read each line of the file
-//         std::vector<std::any> row_data;  
-//         std::vector<std::string> row;    
-//         std::stringstream ss(line);      
-//         std::string cell;                 
-//         while (std::getline(ss, cell, delimiter)) {  // Parse each cell using delimiter
-//             row.push_back(cell);                    
-//         }
-//         if (header) {                             // If it's the first row (header)
-//             for (const auto& col : row) {         
-//                 column_order.push_back(col);      // Add column name to column_order vector
-//             }
-//             header = false;                        
-//         } else {                                  // For subsequent rows (data)
-//             for (size_t i = 0; i < row.size(); ++i) { 
-//                 if (i < types.size() && types[i] != nullptr) {  
-//                     // Check the type of the cell and convert it accordingly
-//                     if (*types[i] == typeid(int)) {
-//                         row_data.push_back(std::stoi(row[i]));   
-//                     } else if (*types[i] == typeid(float)) {
-//                         row_data.push_back(std::stof(row[i]));   
-//                     } else if (*types[i] == typeid(std::string)) {
-//                         row_data.push_back(row[i]);               
-//                     } else {
-//                         //TODO: Add more types as needed (data)
-//                         row_data.push_back(row[i]);               
-//                     }
-//                 } else {
-//                     row_data.push_back(row[i]);                   // Default to string
-//                 }
-//             }
-//             data.push_back(row_data);             // Add the row data to the main data vector
-//         }
-//     }
-
-//     file.close();                        
-//     return {column_order, data};         
-// }
-
-// std::pair<std::vector<std::string>, std::vector<std::vector<std::any>>> TXTReader::read(const std::string& filename, const std::vector<const std::type_info*>& types, char delimiter) {
-//     /*
-//         Function to read TXT file and return data as a pair of vectors
-//     */
-//     std::ifstream file(filename);  // Open the TXT file for reading
-//     if (!file.is_open()) {          // Check if file opening failed
-//         std::cerr << "Error opening TXT file: " << filename << std::endl;  
-//         return {};                  
-//     }
-
-//     std::vector<std::string> column_order;        // Vector to store column names
-//     std::vector<std::vector<std::any>> data;      // Vector of vectors to store data
+    std::string line;               // String para armazenar cada linha do arquivo TXT
+    bool header = true;             
     
-//     std::string line;               // String to store each line of the TXT file
-//     bool header = true;  
-//     while (std::getline(file, line)) {  // Read each line of the file
-//         std::vector<std::any> row_data;  
-//         std::vector<std::string> row;    
-//         std::stringstream ss(line);      
-//         std::string cell;                 
-//         while (std::getline(ss, cell, delimiter)) {  // Parse each cell using delimiter
-//             row.push_back(cell);                    
-//         }
-//         if (header) {                             // If it's the first row (header)
-//             for (const auto& col : row) {         
-//                 column_order.push_back(col);      // Add column name to column_order vector
-//             }
-//             header = false;                        
-//         } else { 
-//             for (size_t i = 0; i < row.size(); ++i) { 
-//                 if (i < types.size() && types[i] != nullptr) {  
-//                     // Check the type of the cell and convert it accordingly
-//                     if (*types[i] == typeid(int)) {
-//                         row_data.push_back(std::stoi(row[i]));   
-//                     } else if (*types[i] == typeid(float)) {
-//                         row_data.push_back(std::stof(row[i]));   
-//                     } else if (*types[i] == typeid(std::string)) {
-//                         row_data.push_back(row[i]);               
-//                     } else {
-//                         //TODO: Add more types as needed (data)
-//                         row_data.push_back(row[i]);               
-//                     }
-//                 } else {
-//                     row_data.push_back(row[i]);                   // Default to string
-//                 }
-//             }
-//             data.push_back(row_data);             // Add the row data to the main data vector
-//         }
-//     }
-//     file.close();                        
-//     return {column_order, data};         
-// }
+    size_t line_count = 0;  // Contador de linhas lidas
+    while (std::getline(file, line)) {  // Lê cada linha do arquivo
+        std::vector<std::any> row_data;  // Vetor para armazenar dados para a linha atual
+        std::vector<std::string> row;    // Vetor para armazenar elementos individuais da linha atual
+        std::stringstream ss(line);      // String stream para analisar a linha atual
+        std::string cell;                // String para armazenar cada célula da linha atual
+
+        while (std::getline(ss, cell, delimiter)) {  // Analisa cada célula usando o delimitador
+            row.push_back(cell);                    
+        }
+
+        if (header) {                             // Se for a primeira linha (cabeçalho)
+            for (const auto& col : row) {         
+                column_order.push_back(col);      // Adiciona o nome da coluna ao vetor column_order
+            }
+            header = false;                        
+        } else {                                  // Para linhas subsequentes (dados)
+            for (size_t i = 0; i < row.size(); ++i) { 
+                if (i < types.size() && types[i] != nullptr) {  
+                    // Verifica o tipo da célula e converte conforme necessário
+                    if (*types[i] == typeid(int)) {
+                        row_data.push_back(std::stoi(row[i]));   
+                    } else if (*types[i] == typeid(float)) {
+                        row_data.push_back(std::stof(row[i]));   
+                    } else if (*types[i] == typeid(std::string)) {
+                        row_data.push_back(row[i]);               
+                    } else {
+                        //TODO: Adicione mais tipos conforme necessário (dados)
+                        row_data.push_back(row[i]);               
+                    }
+                } else {
+                    row_data.push_back(row[i]);                   // Padrão é string
+                }
+            }
+
+            block_data.push_back(row_data);  // Adiciona os dados da linha ao vetor de dados do bloco
+            line_count++;
+
+            if (line_count == 50 && read_in_blocks) {  // Se o tamanho do bloco for atingido ou final do arquivo
+                // Adiciona os dados do bloco à fila
+                queue.push({column_order, block_data});
+
+                line_count = 0;  // Reinicia o contador de linha
+                block_data.clear();  // Limpa os dados do bloco
+            }
+        }
+    }
+
+    if (!block_data.empty()) {  // Se houver dados restantes
+        queue.push({column_order, block_data});  // Adiciona os dados restantes à fila
+    }
+
+    file.close();                        
+}
--- test_reader.cpp
+++ test_reader.cpp
@@ -8,114 +8,78 @@
 
 
 int main(){ 
-    
-    // Test the CSVReader class
-    // std:: cout << "Test the CSVReader class" << std::endl;
     // CSVReader csvReader;
-
-    // std::vector<const std::type_info*> types = {&typeid(std::string), &typeid(int), &typeid(std::string), &typeid(std::string)};
-
-    // auto [column_order, data] = csvReader.read("../testData/data.csv", types);
+    // std::vector<const std::type_info*> types2 = {&typeid(int), &typeid(std::string), &typeid(std::string)};
     
-    // DataFrame df(column_order, types);
+    // ConsumerProducerQueue<std::pair<std::vector<std::string>, std::vector<std::vector<std::any>>>> queue(15);
+    // csvReader.read("../testData/data2.csv", types2, ',', queue, true);
+    // std::cout << "Queue size: " << queue.size() << std::endl;
+    // // Print elements in the queue
+    // while (!queue.is_empty()) {
+    //     //print size of the queue
+    //     std::cout << "Queue size: " << queue.size() << std::endl;
 
-    // for (const auto& row : data) {
-    //     df.add_row(row);
+    //     auto [column_order, block_data] = queue.pop();
+
+    //     // convert it to a DataFrame
+    //     DataFrame df(column_order, types2);
+    //     for (const auto& row : block_data) {
+    //         df.add_row(row);
+    //     }
+
+    //     auto v1 = df.get_column<int>("id");
+    //     auto v2 = df.get_column<std::string>("semana");
+    //     auto v3 = df.get_column<std::string>("data");
+
+    //     for (size_t i = 0; i < v1.size(); i++) {
+    //         std::cout << v1[i] << " - " << v2[i] << " - " << v3[i] << std::endl;
+    //     }
+
     // }
 
-    // auto v1 = df.get_column<std::string>("Nome");
-    // for (const auto& i : v1) {
-    //     std::cout << i << "<--";
-    //     std::cout << typeid(i).name() << std::endl;
-    // }
+    // std::cout << "Test without blocks" << std::endl;
 
-    // auto v2 = df.get_column<int>("Idade");
-    // for (const auto& i : v2) {
-    //     std::cout << i << "<--";
-    //     std::cout << typeid(i).name() << std::endl;
-    // }
+    // ConsumerProducerQueue<std::pair<std::vector<std::string>, std::vector<std::vector<std::any>>>> queue1(15);
+    // csvReader.read("../testData/data2.csv", types2, ',', queue1, false);
+    // std::cout << "Queue size: " << queue1.size() << std::endl;
 
-    // auto v3 = df.get_column<std::string>("Cidade");
-    // for (const auto& i : v3) {
-    //     std::cout << i << "<--";
-    //     std::cout << typeid(i).name() << std::endl;
-    // }
-    
-    
-    // // Test the TXTReader class    
-    // std:: cout << "Test the TXTReader class" << std::endl;
-    // TXTReader txtReader;
+    // std::cout << "Test with TXTReader" << std::endl;
 
-    // auto [column_order_txt, data_txt] = txtReader.read("../testData/data.txt", types);
+    TXTReader txtReader;
+    std::vector<const std::type_info*> types3 = {&typeid(int), &typeid(int), &typeid(std::string)};
+    // std::vector<const std::type_info*> types3 = {&typeid(std::string), &typeid(int), &typeid(std::string), &typeid(std::string)};
+    ConsumerProducerQueue<std::pair<std::vector<std::string>, std::vector<std::vector<std::any>>>> queue2(15);
+    txtReader.read("../testData/data2.txt", types3, '	', queue2, true);
+    std::cout << "Queue size: " << queue2.size() << std::endl;
 
-    // DataFrame df2(column_order_txt, types);
-
-    // for (const auto& row : data_txt) {
-    //     df2.add_row(row);
-    // }
-
-    // auto v4 = df2.get_column<std::string>("Nome");
-    // for (const auto& i : v4) {
-    //     std::cout << i << "<--";
-    //     std::cout << typeid(i).name() << std::endl;
-    // }
-
-    // auto v5 = df2.get_column<int>("Idade");
-
-    // for (const auto& i : v5) {
-    //     std::cout << i << "<--";
-    //     std::cout << typeid(i).name() << std::endl;
-    // }
-
-    // auto v6 = df2.get_column<std::string>("Cidade");
-
-    // for (const auto& i : v6) {
-    //     std::cout << i << "<--";
-    //     std::cout << typeid(i).name() << std::endl;
-    // }
-
-    CSVReader csvReader;
-    std::vector<const std::type_info*> types2 = {&typeid(int), &typeid(std::string), &typeid(std::string)};
-    
-    ConsumerProducerQueue<std::pair<std::vector<std::string>, std::vector<std::vector<std::any>>>> queue(15);
-    // std::vector<const std::type_info*> types2 = {&typeid(std::string), &typeid(int), &typeid(std::string), &typeid(std::string)};
-    
-    csvReader.read("../testData/data2.csv", types2, ',', queue);
-    std::cout << "Queue size: " << queue.size() << std::endl;
-    // Print elements in the queue
-    while (!queue.is_empty()) {
+    while (!queue2.is_empty()) {
         //print size of the queue
-        std::cout << "Queue size: " << queue.size() << std::endl;
+        std::cout << "Queue size: " << queue2.size() << std::endl;
 
-        auto [column_order, block_data] = queue.pop();
-        
-        // print column names
-        for (const auto& col : column_order) {
-            std::cout << "column: " << col << std::endl;
-        }
+        auto [column_order, block_data] = queue2.pop();
 
         // convert it to a DataFrame
-        DataFrame df(column_order, types2);
+        DataFrame df(column_order, types3);
         for (const auto& row : block_data) {
             df.add_row(row);
         }
 
         auto v1 = df.get_column<int>("id");
-        auto v2 = df.get_column<std::string>("data");
+        auto v2 = df.get_column<int>("dv");
+        auto v3 = df.get_column<std::string>("iv");
 
         for (size_t i = 0; i < v1.size(); i++) {
-            std::cout << v1[i] << " - " << v2[i] << std::endl;
+            std::cout << v1[i] << " - " << v2[i]  << " - " << v3[i] << std::endl;
         }
 
-        // TODO: This is giving an error for some reason...
-
-        // auto v3 = df.get_column<std::string>("diasemana");
-        // for (const auto& i : v3) {
-        //     std::cout << i << "<--";
-        //     std::cout << typeid(i).name() << std::endl;
-        // }
-
     }
+
+    std::cout << "Test without blocks" << std::endl;
+
+    ConsumerProducerQueue<std::pair<std::vector<std::string>, std::vector<std::vector<std::any>>>> queue3(15);
+    txtReader.read("../testData/data2.txt", types3, ' ', queue3, false);
+    std::cout << "Queue size: " << queue3.size() << std::endl;
+
 
     std::cout << "End of test" << std::endl;
 
