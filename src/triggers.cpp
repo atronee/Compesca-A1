f8925a47adecc846866a201f067cc5a1bd6e275c
Otávio Augusto <otavio.oam@protonmail.com>
2024-04-20 11:25:08 -0300

    Make eventBasedTrigger work

--- src/triggers.cpp
+++ src/triggers.cpp
@@ -3,19 +3,16 @@
 #include <thread>
 #include <filesystem>
 #include "triggers.h"
+#include <set>
 
-//Utility
-bool isNewLogFile(const std::filesystem::path& folder, const std::string& filename) {
-    // Check if the file exists in the folder and is not empty
-    std::filesystem::path filePath = folder / filename;
-    return std::filesystem::exists(filePath) && std::filesystem::file_size(filePath) > 0;
-}
+
 // Child class TimeBasedTrigger
 void TimeBasedTrigger::executePipeline(){
 // Your time-based pipeline execution logic here
     std::cout << "Time-based trigger: Pipeline executed\n";
 }
-void TimeBasedTrigger::startTrigger(const std::chrono::seconds& interval) {
+void TimeBasedTrigger::startTrigger(int seconds) {
+    const auto interval = std::chrono::seconds(seconds);
     while (true) {
         executePipeline();
         std::this_thread::sleep_for(interval);
@@ -28,6 +25,11 @@ void EventBasedTrigger::executePipeline(){
     std::cout << "Event-based trigger: Pipeline executed\n";
 }
 
+bool EventBasedTrigger::isNewLogFile(const std::filesystem::path& folder, const std::string& filename) {
+    // Check if the file is not in the set of processed files
+    return processedFiles.find(filename) == processedFiles.end();
+}
+
 void EventBasedTrigger::triggerOnApperanceOfNewLogFile(const std::filesystem::path logFolder) {
     while (true) {
         // Check for new log files every 5 seconds
@@ -36,8 +38,11 @@ void EventBasedTrigger::triggerOnApperanceOfNewLogFile(const std::filesystem::pa
 
         // Iterate through files in the log folder
         for (const auto& entry : std::filesystem::directory_iterator(logFolder)) {
-            if (entry.is_regular_file() && isNewLogFile(logFolder, entry.path().filename())) {
-                // Trigger pipeline execution for the new log file
+            // check if file is not in the set of processed files and is a txt file
+            if (entry.path().extension() == ".txt" && isNewLogFile(logFolder, entry.path().filename().string())){
+                // Process the new log file
+                std::cout << "New log file detected: " << entry.path().filename() << std::endl;
+                processedFiles.insert(entry.path().filename().string());
                 executePipeline();
             }
         }
@@ -48,12 +53,9 @@ int main() {
     TimeBasedTrigger timeTrigger;
     EventBasedTrigger eventTrigger;
 
-    // Start time-based trigger every 5 seconds
-    // const auto interval = std::chrono::seconds(5);
-    // timeTrigger.startTrigger(interval);
+    // timeTrigger.startTrigger(5);
 
-    // Trigger event-based pipeline execution
-    eventTrigger.triggerOnApperanceOfNewLogFile("./");
+    eventTrigger.triggerOnApperanceOfNewLogFile("./logs");
 
 
     return 0;
--- src/triggers.h
+++ src/triggers.h
@@ -4,22 +4,27 @@
 #include <iostream>
 #include <chrono>
 #include <filesystem>
+#include <set>
 
 class Trigger {
         public:
         virtual void executePipeline() = 0; // Pure virtual function for pipeline execution
+        // list of processed files
+        std::set<std::string> processedFiles;
 };
 
 class TimeBasedTrigger : public Trigger {
 public:
     void executePipeline() override;
 
-    void startTrigger(const std::chrono::seconds &interval);
+    void startTrigger(int seconds);
 };
 
 class EventBasedTrigger : public Trigger {
 public:
     void executePipeline() override;
+
+    bool isNewLogFile(const std::filesystem::path& folder, const std::string& filename);
 
     void triggerOnApperanceOfNewLogFile(const std::filesystem::path logFolder);
 };
