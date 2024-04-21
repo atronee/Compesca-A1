c322793548730405efd3d081f4cc994b6508e8d2
Luís Felipe Marques <marques.luis@fgv.edu.br>
2024-04-21 15:26:02 -0300

    Adicione handlers de group by e sort

--- src/Handlers.cpp
+++ src/Handlers.cpp
@@ -194,6 +194,103 @@ void FilterHandler::filter(string column, string operation, string value) {
     }
 }
 
+void GroupByHandler::group_by(string column, string operation) {
+    while(true) {
+        DataFrame* df = queue_in->pop();
+        if (df == nullptr) {
+            queue_out->push(nullptr);
+            break;
+        }
+        if (df->get_column_type(column) == type_to_index[std::type_index(typeid(int))]) {
+            vector<int> column_data = df->get_column<int>(column);
+            std::sort(column_data.begin(), column_data.end());
+            vector<int> new_column_data;
+            if (operation == "sum") {
+                int sum = 0;
+                for (size_t i = 0; i < column_data.size(); ++i) {
+                    sum += column_data[i];
+                }
+                new_column_data.push_back(sum);
+            } else if (operation == "mean") {
+                int sum = 0;
+                for (size_t i = 0; i < column_data.size(); ++i) {
+                    sum += column_data[i];
+                }
+                new_column_data.push_back(sum / column_data.size());
+            } else if (operation == "max") {
+                new_column_data.push_back(column_data.back());
+            } else if (operation == "min") {
+                new_column_data.push_back(column_data.front());
+            } else {
+                throw std::invalid_argument("Invalid operation");
+            }
+            df->update_column(column, new_column_data);
+        } else if (df->get_column_type(column) == type_to_index[std::type_index(typeid(float))]) {
+            vector<float> column_data = df->get_column<float>(column);
+            std::sort(column_data.begin(), column_data.end());
+            vector<float> new_column_data;
+            if (operation == "sum") {
+                float sum = 0;
+                for (size_t i = 0; i < column_data.size(); ++i) {
+                    sum += column_data[i];
+                }
+                new_column_data.push_back(sum);
+            } else if (operation == "mean") {
+                float sum = 0;
+                for (size_t i = 0; i < column_data.size(); ++i) {
+                    sum += column_data[i];
+                }
+                new_column_data.push_back(sum / column_data.size());
+            } else if (operation == "max") {
+                new_column_data.push_back(column_data.back());
+            } else if (operation == "min") {
+                new_column_data.push_back(column_data.front());
+            } else {
+                throw std::invalid_argument("Invalid operation");
+            }
+            df->update_column(column, new_column_data);
+        } else {
+            throw std::invalid_argument("Invalid column type");
+        }
+        queue_out->push(df);
+    }
+}
+
+void SortHandler::sort(string column, string order) {
+    while(true) {
+        DataFrame* df = queue_in->pop();
+        if (df == nullptr) {
+            queue_out->push(nullptr);
+            break;
+        }
+        if (df->get_column_type(column) == type_to_index[std::type_index(typeid(int))]) {
+            vector<int> column_data = df->get_column<int>(column);
+            if (order == "asc") {
+                std::sort(column_data.begin(), column_data.end());
+            } else if (order == "desc") {
+                std::sort(column_data.begin(), column_data.end(), std::greater<int>());
+            } else {
+                throw std::invalid_argument("Invalid order");
+            }
+            df->update_column(column, column_data);
+        } else if (df->get_column_type(column) == type_to_index[std::type_index(typeid(float))]) {
+            vector<float> column_data = df->get_column<float>(column);
+            if (order == "asc") {
+                std::sort(column_data.begin(), column_data.end());
+            } else if (order == "desc") {
+                std::sort(column_data.begin(), column_data.end(), std::greater<float>());
+            } else {
+                throw std::invalid_argument("Invalid order");
+            }
+            df->update_column(column, column_data);
+        } else {
+            throw std::invalid_argument("Invalid column type");
+        }
+        queue_out->push(df);
+    }
+}
+
+
 void printHandler::print() {
     while(true) {
         DataFrame* df = queue_in->pop();
