485def674e5499fe9c361ac40ae75ffb88264789
Vinícius Sousa <vinicius.antunes@proton.me>
2024-05-16 10:54:49 -0300

    join Cmake Files, add potry files and update mock_client

--- rpc/mock_client.py
+++ rpc/mock_client.py
@@ -10,29 +10,27 @@ import contract_pb2_grpc
 import mock
 
 
-def guide_log_message(stub, user_author_id, action, button_product_id, stimulus, component, text_content, date):
-    log_message = contract_pb2.LogMessage(user_author_id=user_author_id, action=action, button_product_id=button_product_id, stimulus=stimulus, component=component, text_content=text_content, date=date)
-    response = stub.LogUserBehavior(iter([log_message]))
+def guide_log_message(stub, num_messages: int):
+    log_message = []
+    for _ in range(num_messages):
+        message = (mock.generateLogUserBehavior())
+        log_message.append(contract_pb2.LogMessage(user_author_id=message["user_author_id"],
+                                                   action=message["action"],
+                                                   button_product_id=message["button_product_id"],
+                                                   stimulus=message["stimulus"],
+                                                   component=message["component"],
+                                                   text_content=message["text_content"],
+                                                   date=str(message["date"])))
+
+
+    response = stub.LogUserBehavior(iter(log_message))
     print("LogMessage client received: " + response.response)
 
 
 def run():
     with grpc.insecure_channel('localhost:50051') as channel:
         stub = contract_pb2_grpc.LogServiceStub(channel)
-        for _ in range(10):
-            log_message = mock.generateLogUserBehavior()
-            user_author_id = int(log_message["user_author_id"])
-            action = log_message["action"]
-            button_product_id = int(log_message["button_product_id"])
-            stimulus = log_message["stimulus"]
-            component = log_message["component"]
-            text_content = log_message["text_content"]
-            date = str(log_message["date"])
-
-            guide_log_message(stub, user_author_id, action,
-                            button_product_id, stimulus,
-                            component, text_content, date)
-            
+        guide_log_message(stub, 10)
 
 if __name__ == "__main__":
     logging.basicConfig()
